332fcff4d762eb677d8d84eb8c6f36b1a44e1c28
wilmerwave <wilmer.cantillo@correounivalle.edu.co>
2023-04-18 18:25:40 -0500

    first commit

---
+++ src/index.js
@@ -0,0 +1,10 @@
+const app = require ('./app');
+
+async function main(){
+    await app.listen(3000);
+    console.log('Server funcionando')
+}
+
+app.listen(3000, () => console.log('server on port 3000'))
+
+main();
